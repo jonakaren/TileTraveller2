5996057177d976cb8d44ce5851b7570717ae133a
hrefnamarie <hrefnag20@ru.is>
2020-09-17 10:14:57 +0000

    algorithm  added

---
+++ TileTraveller.py
@@ -0,0 +1,6 @@
+#  Give a variable the name "Starting Tile" 
+#  Give option of movement.
+#  When user moves the variable changes to the tile he is placed in and the loop starts again.
+#  if user tries to use movement not available print ("Not a valid direction")
+#  When user arrives in tile 3.1 print ("Victory") and end the loop.
+
